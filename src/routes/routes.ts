9bfab7481aba6d3f0e2a37843a6195d7f33768de
KirdesMF <cedgourville@gmail.com>
2022-11-01 20:58:45 +0100

    fix: route vercel

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,8 +1,10 @@
 import { createRouter, createWebHistory } from "vue-router";
+import Home from "../pages/PageHome.vue";
+import Invite from "../pages/PageInvite.vue";
 
 export const routes = [
-  { path: "/", component: import("../pages/PageHome.vue") },
-  { path: "/invite", component: import("../pages/PageInvite.vue") },
+  { path: "/", component: Home },
+  { path: "/invite", component: Invite },
 ];
 
 export const router = createRouter({
